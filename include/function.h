d025001d3d0800247c49ba1daf2e84cdece0aa4f
Tiago Onofre <taraujonofre@gmail.com>
2019-09-26 23:24:22 -0300

    reverse and copy

--- include/function.h
+++ include/function.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <functional>
 #include <algorithm>
+#include <iterator>
 #include <utility>
 
 /*
@@ -70,7 +71,43 @@ std::pair< Itr, Itr > minmax( const Itr first, const Itr last, Compare cmp )
 
 
 //==================================================================================
-//function 
+//function copy - return last iterator of the new range
+//==================================================================================
 
+template< typename Itr >
+
+Itr copy( const Itr first, const Itr last, Itr dfirst )
+{
+    Itr fast = first;
+
+    while( fast != last )
+    {
+        *dfirst++ = *fast++;
+    }
+
+    return dfirst;
+}
+//=================================================================================
+
+
+
+//=================================================================================
+//function reverse - invert elements in range
+//=================================================================================
+
+template< typename Itr >
+
+void reverse( Itr first, Itr last )
+{
+    Itr middle = first + std::distance( first, last)/2 - 1;
+
+    last--;
+
+    while(  first <= middle  )
+    {
+        std::iter_swap( first++, last-- );
+    }
+}
+//=================================================================================
 #endif
 
--- src/app_func.cpp
+++ src/app_func.cpp
@@ -28,8 +28,72 @@ int main ( void )
 
 
 
-//===================== TEST Reverse =========================
+//===================== TEST Copy ==========================
 
+    int B[10];
+
+    std::cout << ">>>Vetor B antes de copy:\n";
+    std::cout << "\n[ ";
+    for( const int & i: B )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
+
+    int * Blast = B + 10;
+
+    Blast = copy( A, A+10, B );
+
+    std::cout << ">>>Vetor B depois de copy:\n";
+    std::cout << "\n[ ";
+    for( const int & i: B )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
+
+    std::cout << ">>>O Blast deve imprimit um lixo de memória: " << *Blast << "\n"; 
+
+
+//===================== TEST Reverse ==========================
+
+    int C[10] = { 1, 2 ,3, 4, 5, 6, 7, 8, 9, 10 };
+    int D[5] = { 1, 3, 5, 7, 9 };
+
+    std::cout << ">>>Vetor C antes de reverse:\n";
+    std::cout << "\n[ ";
+    for( const int & i: C )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
+
+    std::cout << ">>>Vetor D antes de reverse:\n";
+    std::cout << "\n[ ";
+    for( const int & i: D )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
+
+    reverse( C, C + 10 );
+    reverse( D, D + 5 );
+
+    std::cout << ">>>Vetor C depois de reverse:\n";
+    std::cout << "\n[ ";
+    for( const int & i: C )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
+
+    std::cout << ">>>Vetor D depois de reverse:\n";
+    std::cout << "\n[ ";
+    for( const int & i: D )
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "]\n\n";
 
     return 0;
 }
